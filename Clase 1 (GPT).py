458194f7deae58bdd39c08cb1d4c5d301ecec728
juako615 <juako615@gmail.com>
2026-08-01 00:10:25 +1200

    Commit inicial

---
+++ Clase 1 (GPT).py
@@ -0,0 +1,5 @@
+print("mi nombre es joaquin") #nombre 
+print("tengo 30 años") #edad
+print("vivo en nueva zelanda") #ubicación
+print("estoy aprendiendo a programar en phyton") #actividad
+print("mi objetivo es trabajar en data science") #objetivo
---
+++ Clase 2 (video).py
@@ -0,0 +1,12 @@
+suma= 5+6 #operador suma
+print(suma)
+modulo= 10%3 #operador modulo
+print(modulo)
+potencia= 5**3 #operador potencia
+print(potencia)
+division_entera = 9//2 #operador division entera
+print(division_entera)
+nombre=5
+type(nombre) #operador type
+nombre= "joaquin"
+type(nombre) #operador type
